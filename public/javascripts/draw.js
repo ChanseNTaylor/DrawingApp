b66ed8acdba79fd0a089cc5004522510c94c3b9a
Chanse Taylor <Chansen.taylor@gmail.com>
2020-02-11 00:30:49 -0500

    Added basic drawing functionality

--- public/javascripts/draw.js
+++ public/javascripts/draw.js
@@ -1,10 +1,51 @@
 "use strict";
-const ctx = document.getElementsByClassName("canvas")[0].getContext("2d");
+const canvas = document.getElementsByClassName("canvas")[0];
 
-const clearToColor = (colorStr = "white") =>
+if(canvas.getContext("2d"))
 {
-    ctx.fillStyle = colorStr;
-    ctx.fillRect(0, 0, ctx.canvas.clientWidth, ctx.canvas.clientHeight);
-}
+    let x;
+    let y;
+    let drag = false;
+    const ctx = canvas.getContext("2d");
 
-clearToColor();
+    const clearToColor = (colorStr = "white") =>
+    {
+        ctx.fillStyle = colorStr;
+        ctx.fillRect(0, 0, ctx.canvas.clientWidth, ctx.canvas.clientHeight);
+    }
+
+    const resizeCanvas = (width, height) =>
+    {
+        canvas.width = width;
+        canvas.height = height;
+    }
+
+    canvas.addEventListener("mousedown", () =>
+    {
+        drag = true;
+
+        ctx.beginPath();
+        ctx.moveTo(x, y);
+    });
+
+    canvas.addEventListener("mousemove", evt =>
+    {
+        y = evt.y - canvas.offsetTop;
+        x = evt.x - canvas.offsetLeft;
+
+        if(drag)
+        {
+            ctx.lineTo(x, y);
+            ctx.stroke();
+        }
+    });
+
+    canvas.addEventListener("mouseup", evt => { drag = false; });
+
+    resizeCanvas(800, 500);
+    clearToColor();
+}
+else
+{
+    window.alert("Your browser does not support the canvas element.");
+}
--- routes/explore.js
+++ routes/explore.js
@@ -1,7 +1,7 @@
+"use strict";
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
 router.get('/', function(req, res, next) { res.render("explore"); });
 
 module.exports = router;
